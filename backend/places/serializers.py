adf347094cb1444065c57d2cf07d8a87a7a93b00
hiheeen <gmldms0338@naver.com>
2023-07-07 10:30:50 +0900

    backend 폴더 삭제

--- backend/places/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Place
-from users.serializers import UserSerializer
-
-class PlaceSerializer(ModelSerializer):
-    user = UserSerializer(read_only=True)
-    class Meta:
-        model = Place
-        fields = "__all__"
